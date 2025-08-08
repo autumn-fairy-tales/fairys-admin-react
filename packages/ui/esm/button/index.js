08db066719157bb5cc2c2e03ac22490fc243522c
SunLxy <1011771396@qq.com>
2025-08-08 09:41:09 +0800

    fix:修改

--- packages/ui/esm/button/index.js
+++
@@ -1,6 +0,0 @@
-import { jsx } from "react/jsx-runtime";
-const Button = ()=>/*#__PURE__*/ jsx("button", {
-        className: "bg-red-500 text-white",
-        children: "\u6309\u94AE"
-    });
-export { Button };
