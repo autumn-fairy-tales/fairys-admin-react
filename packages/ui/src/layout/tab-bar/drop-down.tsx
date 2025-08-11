774ba6df14311da49284c98d0dcfeb1ddad582a7
SunLxy <1011771396@qq.com>
2025-08-11 20:03:45 +0800

    feat: 完善tabBar菜单下拉选择

--- packages/ui/src/layout/tab-bar/drop-down.tsx
+++ packages/ui/src/layout/tab-bar/drop-down.tsx
@@ -1,27 +1,37 @@
 import { Popover, PopoverButton, PopoverPanel, useClose } from '@headlessui/react';
 import { Fragment, useMemo } from 'react';
-import { useTabBar, TabBarItemType } from '../../context/tab-bar';
+import { useTabBar, TabBarItemType, tabBarInstance } from '../../context/tab-bar';
 import { Icon } from '@iconify/react';
-import { useNavigate } from 'react-router';
+import { useNavigate, matchPath, useLocation } from 'react-router';
 
 const Items = () => {
   const close = useClose();
   const [state] = useTabBar();
   const navigate = useNavigate();
   const list = state.dropDownTabBarItems;
+  const location = useLocation();
 
-  const onNativeClick = (item: TabBarItemType) => {
+  const onNativeClick = (item: TabBarItemType, e: React.MouseEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
     navigate(item.path);
     close();
+  };
+
+  const onClose = (item: TabBarItemType, e: React.MouseEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
+    const match = matchPath(item.path, location.pathname);
+    tabBarInstance.remove(item.path, !!match, navigate);
   };
 
   const render = useMemo(() => {
     return (list || []).map((item) => {
       return (
         <div
-          onClick={() => onNativeClick(item)}
+          onClick={(e) => onNativeClick(item, e)}
           key={item.path}
-          className="shrink-0 text-gray-400 transition-[background-color] flex flex-row items-center gap-1 px-[20px] py-[10px] cursor-pointer"
+          className="shrink-0 text-gray-400 hover:bg-gray-100 transition-[background-color] flex flex-row items-center gap-1 px-[20px] py-[10px] cursor-pointer"
         >
           {item.icon ? (
             <span className="size-[16px]">
@@ -31,6 +41,10 @@ const Items = () => {
             <Fragment />
           )}
           <div className="flex-1  whitespace-nowrap">{item.title}</div>
+          <span
+            className="icon-[ant-design--close-outlined] ml-5 text-gray-400 hover:text-gray-600"
+            onClick={(e) => onClose(item, e)}
+          />
         </div>
       );
     });
@@ -50,14 +64,14 @@ export const DropDownTabBarItems = () => {
     }
     return (
       <Fragment>
-        <Popover className="h-full border-s border-gray-200">
-          <PopoverButton className="h-full text-sm/6 font-semibold focus:outline-none w-[50px] text-gray-400 hover:text-gray-600 cursor-pointer">
-            <span className="icon-[ant-design--caret-down-outlined] " />
+        <Popover className="h-full  flex items-center justify-center w-[50px]">
+          <PopoverButton className="w-[25px] h-[25px] text-sm/6 font-semibold focus:outline-none  bg-white text-gray-400 hover:text-gray-600 cursor-pointer">
+            <span className="icon-[ant-design--caret-down-outlined]" />
           </PopoverButton>
           <PopoverPanel
             transition
-            anchor={{ to: 'bottom start', gap: '4px' }}
-            className="rounded-sm  bg-white shadow-xl text-sm/6 transition duration-200 ease-in-out [--anchor-gap:--spacing(5)] data-closed:-translate-y-1 data-closed:opacity-0"
+            anchor={{ to: 'left start', gap: '4px' }}
+            className="rounded-sm bg-white shadow-xl inset-shadow-sm text-sm/6 transition duration-200 ease-in-out [--anchor-gap:--spacing(5)] data-closed:-translate-y-1 data-closed:opacity-0"
           >
             <Items />
           </PopoverPanel>
