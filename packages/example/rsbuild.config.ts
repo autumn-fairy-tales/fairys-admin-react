e5b2a5616dd72b19096202b4e9c6462427e1e4c4
SunLxy <1011771396@qq.com>
2025-08-28 20:41:23 +0800

    feat: 添加案例

--- packages/example/rsbuild.config.ts
+++ packages/example/rsbuild.config.ts
@@ -3,6 +3,9 @@ import { pluginReact } from '@rsbuild/plugin-react';
 import tailwindcss from '@tailwindcss/postcss';
 
 export default defineConfig({
+  output: {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/fairys-admin-react/' : '/',
+  },
   html: {
     title: 'Fairys Admin React',
     favicon: './public/logo.png',
