caac9e74de31437198f0e5257885f18fd4da4516
SunLxy <1011771396@qq.com>
2026-03-03 15:12:43 +0800

    style:样式调整

--- packages/ui/src/main-menu/index.tsx
+++ packages/ui/src/main-menu/index.tsx
@@ -88,7 +88,7 @@ const MainMenuItems = (props: MainMenuProps) => {
 
   return (
     <FairysMenu
-      className="fairys_admin_main_menu_items"
+      className="fairys_admin_main_menu_items fairys:w-full fairys:p-0!"
       {...propsConfig}
       activeMotionPrefixCls="fairys-main-menu-item-active"
       selectedKey={state.mainMenuItemSelected}
